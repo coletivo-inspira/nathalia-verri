dce2a43df5d8e5fe6982de79d46e95cdeac09710
Gabriel Atene Silva <72166154+gabrielatenesilva@users.noreply.github.com>
2026-06-22 22:54:53 -0300

    v32

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
 
         {/* A rota "/parcerias" agora vai responder em /parcerias */}
-        <Route path="/nanathsday" element={<BioLink />} />
+        <Route path="/cupons" element={<BioLink />} />
 
         {/* A rota "/parcerias" agora vai responder em /parcerias */}
         <Route path="/manual-de-marca" element={<BrandManual />} />
